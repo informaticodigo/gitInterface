0292fcb5880dd1ac8deee265726f4fb96401d01e
Hugo <hugodomconempresa@gmail.com>
2021-05-23 01:00:29 +0200

    Appended tabs for UI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import system, popen, chdir
 from tkinter import *
+from tkinter.ttk import Notebook
 
 username = popen("echo %USERNAME%").read().replace("\n", "")
 
@@ -9,6 +10,19 @@ def go_path(path):
 
 
 window = Tk()
+window.title("Git Help | InformatiCódigo")
 
+nb = Notebook(window)
+nb.grid(row=0, column=0)
+nb.grid_propagate(False)
+
+neural_networks = Frame(nb)
+data_set = Frame(nb)
+instructions = Frame(nb)
+
+
+nb.add(neural_networks, text="Redes Neuronales")
+nb.add(data_set, text="Preparar el DataSet")
+nb.add(instructions, text="Instrucciones")
 
 window.mainloop()
