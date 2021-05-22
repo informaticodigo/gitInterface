4b40ac3e81f36347ba79744875f39fa72858637b
Hugo <hugodomconempresa@gmail.com>
2021-05-22 22:03:08 +0200

    Initial enviroment + init of Window + some functions

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from os import system
+from os import system, popen, chdir
+from tkinter import *
+
+username = popen("echo %USERNAME%").read().replace("\n", "")
+
+
+def go_path(path):
+    chdir(path)
+
+
+window = Tk()
+
+
+window.mainloop()
