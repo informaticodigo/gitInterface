db6b5b6ac55db682ec2fe839b7d9a89f6268e926
Hugo <hugodomconempresa@gmail.com>
2021-05-23 22:35:33 +0200

    Base for the UI

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from tkinter.ttk import Notebook
 username = popen("echo %USERNAME%").read().replace("\n", "")
 
 
+def is_git_initialized():
+    return "not a git repository" not in popen("git status").read()
+
+
 def go_path(path):
     chdir(path)
 
@@ -12,17 +16,29 @@ def go_path(path):
 window = Tk()
 window.title("Git Help | InformatiCódigo")
 
-nb = Notebook(window)
+menu = Menu(window, tearoff=False)
+menu_file = Menu(menu, tearoff=False)
+menu_file.add_command(label="Open directory", command=lambda: print())
+menu_file.add_command(label="Add .gitignore", command=lambda: print())
+menu.add_cascade(label="File", menu=menu_file)
+window.configure(menu=menu)
+
+nb = Notebook(window, width=1200, height=600)
 nb.grid(row=0, column=0)
 nb.grid_propagate(False)
 
-neural_networks = Frame(nb)
-data_set = Frame(nb)
-instructions = Frame(nb)
+git_actions = Frame(nb)
+git_back_actions = Frame(nb)
+project_line_visualization = Frame(nb)
 
+# ###### Git save actions ##############################
 
-nb.add(neural_networks, text="Redes Neuronales")
-nb.add(data_set, text="Preparar el DataSet")
-nb.add(instructions, text="Instrucciones")
+# ###### Git return back actions ##############################
+
+# ###### Git view project line ##############################
+
+nb.add(git_actions, text="Git save commands")
+nb.add(git_back_actions, text="Git return back commands")
+nb.add(project_line_visualization, text="View project line")
 
 window.mainloop()
